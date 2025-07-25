ae0a336ba81a7be94409dd859137372b3438743b
Shun <shun@formosa1544.com>
2025-07-24 23:39:45 -0700

    Redesign WIP.

--- datainventory/inventory.py
+++ datainventory/inventory.py
@@ -1,95 +1,70 @@
-# Copyright © 2021 by IoT Spectator. All rights reserved.
+# Copyright © 2021, 2025 by IoT Spectator. All rights reserved.
 
 """The main module of Data Inventory."""
 
+import dataclasses
+import datetime
+import enum
 import pathlib
-import shutil
-import sqlalchemy
-import tarfile
 
-import pandas as pd
+from typing import Optional
 
-from sqlalchemy.orm import sessionmaker
 
-from datainventory import _internal_store
-from datainventory import model_store
-from datainventory import media_store
-from datainventory import table_store
+class MediaType(enum.Enum):
+    """Supported media types."""
+
+    Image = enum.auto()
+    Video = enum.auto()
+
+
+@dataclasses.dataclass
+class ImageMetadata:
+    """Metadata for media files."""
+
+    name: str
+
+
+@dataclasses.dataclass
+class VideoMetadata:
+    """Metadata for video files."""
+
+    name: str
+    duration: Optional[datetime.timedelta] = None
+    resolution: Optional[str] = None
+
+
+@dataclasses.dataclass
+class Media:
+    name: str
+    path: pathlib.Path
+    media_type: MediaType
+    size: int
+    metadata: Optional[ImageMetadata | VideoMetadata] = None
+    description: Optional[str] = None
+    inserted_at: Optional[datetime.datetime] = None
 
 
 class Inventory:
-    """Data Inventory."""
 
-    def __init__(self, device_id: str, inventory: pathlib.Path) -> None:
-        self._device_id = device_id
-        self._inventory = inventory
-        self._data_inventory = self._inventory / pathlib.Path("data")
-        self._database_name = f"{self._inventory.name}.db"
-        self._database = self._inventory / self._database_name
+    def __init__(self):
+        pass
 
-        if not self._inventory.exists():
-            self._inventory.mkdir()
-        self._engine = sqlalchemy.create_engine(f"sqlite:///{self._database}")
-        self._metadata = sqlalchemy.MetaData()
-        self._metadata.create_all(bind=self._engine)
+    def insert(self, media: Media):
+        """Insert media data into the inventory."""
+        # Implementation for inserting media data
+        pass
 
-    def get_media_store(self) -> media_store.MediaStore:
-        """Return an instance of the media store."""
-        Session = sessionmaker(bind=self._engine)
-        return media_store.MediaStore(
-            create_key=_internal_store.CREATE_KEY,
-            device_id=self._device_id,
-            session=Session(),
-            connection=self._engine.connect(),
-            data_inventory=self._data_inventory,
-        )
+    def delete(self, name: str):
+        """Delete media data from the inventory by name."""
+        # Implementation for deleting media data
+        pass
 
-    def get_model_store(self) -> model_store.ModelStore:
-        """Return an instance of the model store."""
-        Session = sessionmaker(bind=self._engine)
-        return model_store.ModelStore(
-            create_key=_internal_store.CREATE_KEY,
-            device_id=self._device_id,
-            session=Session(),
-        )
+    def search(self, name: Optional[str] = None) -> list[Media]:
+        """Search media data in the inventory by name."""
+        # Implementation for searching media data
+        return []
 
-    def get_table_store(self) -> table_store.TableStore:
-        """Return an instance of the table store."""
-        Session = sessionmaker(bind=self._engine)
-        return table_store.TableStore(
-            create_key=_internal_store.CREATE_KEY,
-            device_id=self._device_id,
-            metadata=self._metadata,
-            session=Session(),
-            connection=self._engine.connect(),
-        )
-
-    def export(self, dest_filename: pathlib.Path) -> pathlib.Path:
-        """Export the entire data inventory."""
-        self._metadata.reflect(self._engine)
-        session = sessionmaker(bind=self._engine)()
-        tables = list()
-        for table_name in self._metadata.tables:
-            table = self._metadata.tables[table_name]
-            table_csv = self._inventory / pathlib.Path(f"{table}.csv")
-            pd.DataFrame(
-                session.query(table).all(), columns=table.columns.keys()
-            ).to_csv(path_or_buf=table_csv, index=False)
-            tables.append(table_csv)
-
-        archive_path = f"{dest_filename.name}.tar.gz"
-        with tarfile.open(name=archive_path, mode="w:gz") as tarball:
-            tarball.add(name=self._data_inventory)
-            for table_csv_file in tables:
-                tarball.add(name=table_csv_file)
-                table_csv_file.unlink()
-        return pathlib.Path(archive_path)
-
-    def destroy(self) -> None:
-        """Destory the entire data inventory."""
-        if self._inventory:
-            shutil.rmtree(self._inventory)
-
-    def import_data(self, source_data: pathlib.Path) -> None:
-        """Import data into the data inventory."""
-        raise NotImplementedError("This function is not implemented.")
+    def query(self, name: Optional[str] = None) -> list[Media]:
+        """Query media data from the inventory."""
+        # Implementation for querying media data
+        return []
